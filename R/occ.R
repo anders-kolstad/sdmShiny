6de7c1d288f6cd01b92fe8982a45904ddabb5be9
anders-kolstad <anders.kolstad@ntnu.no>
2020-04-17 07:28:29 +0200

    deleted commit history to get rid of large file

---
+++ R/occ.R
@@ -0,0 +1,70 @@
+#' Get occurence data from gbif
+#' 
+#' Take a species list and harvest gbif data
+#' 
+
+
+
+# Forest plant occurences from GBIF
+
+#file <- "/home/anders/Documents/R/Git projects/sdmShiny/raw data/0008843-181108115102211.zip"
+#out <- finch::dwca_read # ERROR finch wont download
+
+#keysL  <-sapply(as.character(herblichen$Vitenskapelig.navn),function(x) name_backbone(x,rank='species')$speciesKey)
+
+#paste(keysL, collapse = ',')#Copy and paste to below
+
+#odLichen <- occ_download('taxonKey = 2609133,2607387,3408876,2603661,2602561,2609349,3422592,3433876,2601243,2608140,5260765,7083298,2605872,7425899,5260770,9198278,5258394,8704544,3397573,3429282,3390552,2609409,2609180,5260761,2607725,8335421,5260565,2599762,3429909,3389602'
+#                        ,'country = NO','hasCoordinate = TRUE',
+#                      user='jamesspeed',pwd='*****',email='*****')
+#occ_download_meta(odLichen)
+#gbif_citation(occ_download_meta(odLichen))# GBIF Occurrence Download https://doi.org/10.15468/dl.pl144i Accessed from R via rgbif (https://github.com/ropensci/rgbif) on 2018-11-26"
+
+
+
+
+
+# test download
+dismo::gbif("Botrychium", "lanceolatum", download = F)
+dismo::gbif("Primula", "scandinavica", download = F)
+
+ps <- dismo::gbif("Primula", "scandinavica", download = T, geo = T, sp = F)
+
+# this will require a for loop
+
+class(ps)
+
+# backup
+PS <- ps
+
+# remove NA's
+w <- which(is.na(ps$lon))
+if(length(w) != 0) ps <- ps[-w,]
+w <- which(is.na(ps$lat))
+if(length(w) != 0) ps <- ps[-w,]
+w <- which(ps$lon == 0)
+if(length(w) != 0) ps <- ps[-w,]
+w <- which(ps$lat == 0)
+if(length(w) != 0) ps <- ps[-w,]
+
+
+ps$species <- 'Primula scandinavica'
+ps <- ps[,c("lon", "lat","species")]
+head(ps)
+sp::coordinates(ps) <- ~lon + lat
+sp::proj4string(ps) <- sp::proj4string(raster::raster())
+#library(mapview)
+mapview::mapview(ps, 
+                 map.types = c("Esri.WorldShadedRelief",
+                               "Esri.WorldImagery"),
+                 cex = 5, lwd = 0,
+                 alpha.regions = 0.5,
+                 col.regions = "blue")
+
+#library(margrittr) # forward pipe
+#leaflet::leaflet(data = bl) %>% 
+#  addTiles(group = "OSM", 
+#  options = providerTileOptions(minZoom = 2, maxZoom = 100)) # %>% 
+#  addCircleMarkers(lat = ~lat, lng = ~lon,
+#                   color = "blue",
+#                   radius = 0.05)
---
+++ R/spList.R
@@ -0,0 +1,46 @@
+#' Get species lists
+#' 
+#' This scripts produces a list of the species that we will quiery gbif for occurence data.
+#' \code(sl()) produces a species list and a dataframe with the same list pluss some more
+#'@import readxl 
+#'@export
+
+
+sl <- function(){
+# for the alpine plants, it a short list, from Speed and Austrheim 2017 Biological Conservation:
+alp_sl <- c("Botrychium lanceolatum",
+        'Comastoma tenellum',
+        'Gentianella campestris',
+        'Kobresia simpliciuscula',
+        'Primula scandinavica',
+        'Pseudorchis albida',
+        'Pulsatilla vernalis')
+
+
+
+# What about synonyms? 
+# Gbif should recognize synonyms through the 'Catalogue of Life. 
+# For example, P. vernalis has 5 synonyms and three infraspecies, 
+# but all data appear when searching for P. vernalis.
+
+
+# Forest plants and lichens species (Speed et al submitted 2020 FEM)
+# This is not actually neccessary as I can also download the DwC archive from the doi cited in th paper. However, that makes it harder to retrace our steps in the future. 
+
+# Note: ABD also offer shapefiles that are updated daily. But we will stick with the more generic solution with gbif which isn't limited to redlisted species.
+
+# The redlist is downloaded from here:
+# https://artsdatabanken.no/Rodliste2015/sok?vurderings%u00e5r=2015
+
+# First, the (v)ascular (f)orest plants (karplanter) in the (r)ed (l)ist:
+vf_rl <- readxl::read_excel("raw data/vasc_rl.xlsx")
+vf_sl<-droplevels(vf_rl[grep('*beit*',vf_rl$Påvirkningsfaktorer,ignore.case=T),])
+vf_sl <- vf_sl[,'Vitenskapelig navn']
+
+#View(vf_rl) 
+# Still quite a long list
+
+mySpList <<- c(alp_sl, vf_sl$`Vitenskapelig navn`)
+mySpListDF <<- data.frame(mySpList, "type" = c(rep("alpine", length(alp_sl)), rep("forest", nrow(vf_sl))))
+
+}
